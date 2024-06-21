9b252d36c37a285fa9a0af9609b770d0ee2eec5c
ANANTHKUMAR P <117973764+Ananth6122@users.noreply.github.com>
2024-06-21 11:25:22 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class reverse{
+    public static void main(String[]args){
+        int []nums=new int[5];
+        Scanner sc=new Scanner(System.in);
+           for(int i=0;i<n;i++){
+            nums[i]=sc.nextInt();
+
+           }
+           for(i=0;i<n;i++){
+            System.out.println(nums[i]);
+           while(nums[i]>0){
+            int dig=nums[i]%10;
+            int sum=0;
+            sum=sum+dig;
+            
+           }
+           System.out.println("sum of digits:"+sum);
+
+
+           }
+}
